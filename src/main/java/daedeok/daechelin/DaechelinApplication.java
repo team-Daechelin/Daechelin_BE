7bb3676fa4035dbecb1cec5b57e51c728e550387
김태균 <taekyun0525@gmail.com>
2025-11-05 20:09:47 +0900

    first commit

---
+++ src/main/java/daedeok/daechelin/DaechelinApplication.java
@@ -0,0 +1,13 @@
+package daedeok.daechelin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaechelinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DaechelinApplication.class, args);
+    }
+
+}
---
+++ src/test/java/daedeok/daechelin/DaechelinApplicationTests.java
@@ -0,0 +1,13 @@
+package daedeok.daechelin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaechelinApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
